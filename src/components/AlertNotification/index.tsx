8686e8c01a98377a7d255c58a71e7ec0379fcded
Eric Bernardo de Sousa <eric.bernardo.sousa@gmail.com>
2024-08-06 11:14:17 -0300

    created test alert notification

--- src/components/AlertNotification/index.tsx
+++ src/components/AlertNotification/index.tsx
@@ -1,8 +1,11 @@
+import { AlertNotificationProps } from '@/types/alertNotification'
 import { ExclamationCircleIcon } from '@heroicons/react/16/solid'
 import { useEffect, useState } from 'react'
-import { AlertProps } from './types'
 
-export function AlertNotification({ message, type = 'success' }: AlertProps) {
+export function AlertNotification({
+  message,
+  type = 'success',
+}: AlertNotificationProps) {
   if (!message) return null
 
   const [visible, setVisible] = useState<boolean>(true)
@@ -21,12 +24,11 @@ export function AlertNotification({ message, type = 'success' }: AlertProps) {
 
   return (
     <div
-      id="alert-1"
+      data-testid="alert-notification"
       className={`flex items-center p-4 mb-4 text-white rounded-lg ${type === 'success' ? 'bg-green' : 'bg-red'} absolute top-3 right-3 transition-opacity duration-300 ${visible ? 'opacity-90' : 'opacity-0'}`}
       role="alert"
     >
       <ExclamationCircleIcon width={20} />
-      <span className="sr-only">Info</span>
       <div className="ms-3 text-sm font-medium">{message}</div>
     </div>
   )
